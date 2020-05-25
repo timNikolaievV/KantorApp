eebc416dd2235b85506433e5bdcd594898df5a11
tima <tima@DESKTOP-VTII9VP>
2020-05-25 21:13:47 +0300

    Initial commit

---
+++ KantorApp/src/main/java/pl/edu/pwr/KantorApp/App.java
@@ -0,0 +1,14 @@
+package pl.edu.pwr.KantorApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        //11
+    }
+}
